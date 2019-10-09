5186d576d56645f4c1c4fca075e61f262d97f05f
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-10-09 20:35:23 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan18-Kelipatan/src/if01/pkg10118016/latihan18/kelipatan/IF0110118016Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan18.kelipatan;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Kelipatan
+ */
+public class IF0110118016Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i*3.5);
+        }
+    }
+    
+}
